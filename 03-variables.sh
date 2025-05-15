16ba1d477fdfd71aa7152642f76a5cc24f60298f
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-15 15:46:02 +0530

    03-variables.sh

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1= Trump
+PERSON2= Musk
+echo "$PERSON1: Hey Hi $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I'm fine. how are you doing"
+echo "$PERSON1: I am fine too. what's up"
+echo "$PERSON2: Nothing, just going to mars"
