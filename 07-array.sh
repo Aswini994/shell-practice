e88fbdaddf6791ad62e357fd8b549451b7c732ac
Aswini994 <aswini.mutyala94@gmail.com>
2025-05-15 21:21:15 +0530

    07-data-types.sh

--- 07-array.sh
+++ 07-array.sh
@@ -5,5 +5,5 @@ MOVIES=("Court" "HIT3" "Thandel" "TEST")
 echo "First Movie: ${MOVIES[2]}"
 echo "First Movie: ${MOVIES[1]}"
 echo "First Movie: ${MOVIES[0]}"
-echo "First Movie: ${MOVIES[4]}"
+echo "First Movie: ${MOVIES[3]}"
 echo "All movies: ${MOVIES[@]}"
