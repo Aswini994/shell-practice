59a121d51aec895a908bae3d7d964f3105f3a8e6
Aswini994 <aswini.mutyala94@gmail.com>
2025-07-31 14:58:06 +0530

    shell-practice

---
+++ loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+ echo "ERROR: please run with root access"
+ exit 1
+else
+    echo "you are running with root access"
+fi
+
+PACKAGES=("mysql","python", "nginx", "nodejs")
+
+VALIDATE(){
+if [ $1 -eq 0]
+then
+    echo "Installing $2 is success"
+else
+    echo "Installing $2 is failed"
+    exit 1
+fi
+
+}
+
+for package in ${PACKAGES[@]}
+do
+dnf list installed $package
+if [ $? -ne 0 ]
+then
+    echo "$package not installed"
+dnf install $package -y
+VALIDATE $? "$package"
+else
+    echo "$package already installed"
+fi
+done
