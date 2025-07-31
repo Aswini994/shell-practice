52739c824bad876e15916350ea393fa0b0925544
Aswini994 <aswini.mutyala94@gmail.com>
2025-07-31 13:10:06 +0530

    shell-practice

--- conditions.sh
+++ conditions.sh
@@ -5,7 +5,6 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
  echo "ERROR: please run with root access"
- exit 1
 else
     echo "you are running with root access"
 fi
@@ -16,5 +15,4 @@ then
     echo "Installing MySql is ... success"
 else
     echo "Installing MySql is failing"
-    exit 1
 fi
